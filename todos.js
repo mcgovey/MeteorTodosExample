b1892101ea3ec31eb3d51dcc630bb93b3b455ffe
Kevin McGovern <kmcg82004@gmail.com>
2015-11-30 12:50:39 -0500

    Tutorial Updates
    Return to blur
    Completed checkbox
    Completed counter

--- todos.js
+++ todos.js
@@ -1,13 +1,34 @@
 Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
-  // code to run on client
+//================Helpers===================
   Template.todos.helpers({
       'todo': function(){
           return Todos.find({}, {sort: {createdAt: -1}});
       }
   });
 
+  Template.todoItem.helpers({
+      'checked': function(){
+          var isCompleted = this.completed;
+          if(isCompleted){
+              return "checked";
+          } else {
+              return "";
+          }
+      }
+  });
+  Template.todosCount.helpers({
+      'totalTodos': function(){
+          return Todos.find().count();
+      },
+      'completedTodos': function(){
+          return Todos.find({ completed: true }).count();
+      }
+  });
+
+
+//===================Events======================
   Template.addTodo.events({
       /// events go here
       'submit form': function(event){
@@ -33,10 +54,24 @@ if (Meteor.isClient) {
           }
       },
       'keyup [name=todoItem]': function(event){
+        if(event.which == 13 || event.which == 27){
+          $(event.target).blur();
+        } else {
           var documentId = this._id;
           var todoItem = $(event.target).val();
           Todos.update({ _id: documentId }, {$set: { name: todoItem }});
-          console.log("Task changed to: " + todoItem);
+        }
+      },
+      'change [type=checkbox]': function(){
+        var documentId = this._id;
+        var isCompleted = this.completed;
+        if(isCompleted){
+            Todos.update({ _id: documentId }, {$set: { completed: false }});
+            console.log("Task marked as incomplete.");
+        } else {
+            Todos.update({ _id: documentId }, {$set: { completed: true }});
+            console.log("Task marked as complete.");
+        }
       }
   });
 
