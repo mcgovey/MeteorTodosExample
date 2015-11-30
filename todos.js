38d09e7c3abecc719eea4f1b41e7eed32e1e86e1
kmcg8 <kmcg82004@gmail.com>
2015-11-29 21:47:49 -0500

    Updated inline editing

--- todos.js
+++ todos.js
@@ -31,6 +31,12 @@ if (Meteor.isClient) {
           if(confirm){
               Todos.remove({ _id: documentId });
           }
+      },
+      'keyup [name=todoItem]': function(event){
+          var documentId = this._id;
+          var todoItem = $(event.target).val();
+          Todos.update({ _id: documentId }, {$set: { name: todoItem }});
+          console.log("Task changed to: " + todoItem);
       }
   });
 
